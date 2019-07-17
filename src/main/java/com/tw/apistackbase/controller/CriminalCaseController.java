1f3692e358b12b25654c08ca2ea5f914be24661e
unknown <11059964896@qq.com>
2019-07-17 23:20:23 +0800

    [GRD] cover Story1-AC5:delete case by case id

--- src/main/java/com/tw/apistackbase/controller/CriminalCaseController.java
+++ src/main/java/com/tw/apistackbase/controller/CriminalCaseController.java
@@ -4,6 +4,7 @@ import com.tw.apistackbase.model.CriminalCase;
 import com.tw.apistackbase.repository.CriminalCaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +33,8 @@ public class CriminalCaseController {
         return criminalCaseRepository.findAllByCaseName(name);
     }
 
-    
+    @DeleteMapping("/criminalcases/{id}")
+    public void deleteCasesById(@PathVariable int id){
+        criminalCaseRepository.deleteById(id);
+    }
 }
