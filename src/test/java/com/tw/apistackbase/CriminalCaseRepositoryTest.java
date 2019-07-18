c9ba3b3541440c7aefb8d942e18f129eb639afc8
GUOED3 <EDDY.GUO@OOCL.COM>
2019-07-18 11:12:47 +0800

    [GRD] test Story1-AC5:delete case by id

--- src/test/java/com/tw/apistackbase/CriminalCaseRepositoryTest.java
+++ src/test/java/com/tw/apistackbase/CriminalCaseRepositoryTest.java
@@ -50,11 +50,24 @@ class CriminalCaseRepositoryTest {
     public void should_return_case_when_find_case_by_name(){
         CriminalCase criminalCase = new CriminalCase("liunan",new Date().getTime());
 
-        criminalCaseRepository.save(criminalCase);
+        CriminalCase criminalCase2 = criminalCaseRepository.save(criminalCase);
 
         List<CriminalCase> criminalCases = criminalCaseRepository.findAllByCaseName(criminalCase.getCaseName());
         CriminalCase cases = criminalCases.get(0);
 
         Assertions.assertEquals(criminalCase.getIncidentTime(),cases.getIncidentTime());
+        Assertions.assertEquals(criminalCase2.getId(),cases.getId());
+        Assertions.assertEquals(criminalCase.getCaseName(),cases.getCaseName());
+    }
+
+    @Test
+    public void should_delete_case_when_given_case_id(){
+        CriminalCase criminalCase = new CriminalCase("liunan",new Date().getTime());
+        CriminalCase criminalCase2 = criminalCaseRepository.save(criminalCase);
+
+        criminalCaseRepository.deleteById(criminalCase2.getId());
+        List<CriminalCase> cases = criminalCaseRepository.findAll();
+
+        Assertions.assertEquals(0,cases.size());
     }
 }
